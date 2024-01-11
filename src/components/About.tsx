855a729e35a79524160db85130518e164f71f49a
sanamerchant <123655714+sanamerchant@users.noreply.github.com>
2024-01-11 13:10:05 -0500

    initial project images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,10 @@
 import styles from "../styles/About.module.css";
 import { Box, Grid, Button, Link } from "@mui/material";
 import { Link as ScrollLink } from "react-scroll";
+import fsabImage from "../images/fsab.png";
+import hackImage from "../images/hack.png";
+import ucaImage from "../images/uca.png";
+import arvasitImage from "../images/arvasit.png";
 
 const About = () => {
   return (
@@ -11,7 +15,7 @@ const About = () => {
             <div className={styles.left_text}>
               <h1 style={{ color: "#167c9e" }}>Hi, I'm Sana Merchant!</h1>
               <p style={{ fontSize: "20px" }}>
-                I'm passionate about the harnessing technology's transformative
+                I'm passionate about harnessing technology's transformative
                 potential.
               </p>
               <p style={{ fontSize: "20px" }}>
@@ -34,59 +38,123 @@ const About = () => {
                     color: "white",
                     fontSize: "15px",
                     padding: "2%",
+                    fontFamily: "Outfit",
+                    width: "20%",
                   }}
                   className={styles.button}
                 >
                   Projects
                 </Button>
               </ScrollLink>
+              <a
+                style={{ textDecoration: "none" }}
+                target="_blank"
+                href="https://drive.google.com/file/d/12OlUldnCOzIZxElj3o6gjMR6wqIgVFir/view?usp=sharing"
+              >
+                <Button
+                  style={{
+                    backgroundColor: "#167c9e",
+                    color: "white",
+                    fontSize: "15px",
+                    padding: "2%",
+                    marginLeft: "5%",
+                    fontFamily: "Outfit",
+                    width: "20%",
+                  }}
+                  className={styles.button}
+                >
+                  Resume
+                </Button>
+              </a>
             </div>
           </Grid>
           <Grid item xs={6}>
-            <div style={{ marginLeft: "35%", lineHeight: 0.4 }}>
+            <div style={{ marginLeft: "20%", lineHeight: 0.4 }}>
               <br />
               <br />
               <br />
-
-              <div className={styles.timeline}>
-                <div className={styles.container_left}>
-                  <div className={styles.content}>
-                    <h2>2017</h2>
-                    <p>Lorem ipsum..</p>
+              <ol className={styles.timeline}>
+                <li className={styles.timeline_item}>
+                  <div className={styles.timeline_item_icon}>
+                    <img height="30px" src={fsabImage} alt="Profile Picture" />
                   </div>
-                </div>
-                <div className={styles.container_right}>
-                  <div className={styles.content}>
-                    <h2>2016</h2>
-                    <p>Lorem ipsum..</p>
+                  <div className={styles.timeline_item_content}>
+                    <br />
+                    <h2 className={styles.headerlink}>Full Stack at Brown</h2>
+                    <p>Website Developer</p>
+                    <p>September 2023 - Present</p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
                   </div>
-                </div>
-              </div>
-              {/* <h2 style={{ color: "#167c9e" }}>University of Central Asia</h2>
-              <p>Data Analyst</p>
-              <p>June - October 2023</p>
-              <br />
-              <h2 style={{ color: "#167c9e" }}>Arvasit Technolabs</h2>
-              <p>Software Engineer Intern</p>
-              <p>May - June 2023</p>
-              <br />
-              <h2 style={{ color: "#167c9e" }}>Hack @ Brown</h2>
-              <p>Front End Developer</p>
-              <p>September 2023 - Present</p>
-              <br />
-              <h2 style={{ color: "#167c9e" }}>Full Stack at Brown</h2>
-              <p>Website Developer</p>
-              <p>September 2023 - Present</p>
-              <p style={{ marginTop: "15%" }}>
-                Click{" "}
-                <a
-                  target="_blank"
-                  href="https://drive.google.com/file/d/12OlUldnCOzIZxElj3o6gjMR6wqIgVFir/view?usp=sharing"
-                >
-                  <b>here</b>
-                </a>{" "}
-                to view my resume
-              </p> */}
+                </li>
+                <li className={styles.timeline_item}>
+                  <div className={styles.timeline_item_icon}>
+                    <img height="30px" src={hackImage} alt="Profile Picture" />
+                  </div>
+                  <div className={styles.timeline_item_content}>
+                    <br />
+                    <a
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      href="https://2024.hackatbrown.org"
+                    >
+                      <h2 className={styles.headerlink}>Hack @ Brown</h2>
+                    </a>
+                    <p>Front End Developer</p>
+                    <p>September 2023 - Present</p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </div>
+                </li>
+                <li className={styles.timeline_item}>
+                  <div className={styles.timeline_item_icon}>
+                    <img height="30px" src={ucaImage} alt="Profile Picture" />
+                  </div>
+                  <div className={styles.timeline_item_content}>
+                    <br />
+                    <a
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      href="https://2024.hackatbrown.org"
+                    >
+                      <h2 className={styles.headerlink}>
+                        University of Central Asia
+                      </h2>
+                    </a>
+                    <p>Data Analyst</p>
+                    <p>June - October 2023</p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </div>
+                </li>
+                <li className={styles.timeline_item}>
+                  <div className={styles.timeline_item_icon}>
+                    <img
+                      height="30px"
+                      src={arvasitImage}
+                      alt="Profile Picture"
+                    />
+                  </div>
+                  <div className={styles.timeline_item_content}>
+                    <br />
+                    <a
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      href="https://2024.hackatbrown.org"
+                    >
+                      <h2 className={styles.headerlink}>Arvasit Technolabs</h2>
+                    </a>
+                    <p>Software Engineer Intern</p>
+                    <p>May - June 2023</p>
+                  </div>
+                </li>
+              </ol>
             </div>
           </Grid>
         </Grid>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,43 +1,30 @@
 import styles from "../styles/Projects.module.css";
 import { Box, Grid } from "@mui/material";
 import projImage from "../images/git.png";
+import nlpImage from "../images/nlp.png";
+import seamImage from "../images/seam.png";
+import shellImage from "../images/shell.png";
+import searchImage from "../images/search.png";
+import plannerImage from "../images/planner.png";
+import treeImage from "../images/tree.png";
+import interpreterImage from "../images/interpreter.png";
+import dbImage from "../images/db.png";
 
 const Projects = () => {
   return (
     <section id="projects">
       <div className={styles.all}>
-        <h1 style={{ marginBottom: "2%", color: "#167c9e" }}>Projects</h1>
+        <h1 style={{ marginBottom: "2%", color: "#167c9e", fontSize: "30px" }}>
+          Projects
+        </h1>
         <div className={styles.outline}>
           <Grid container spacing={3}>
             <Grid item xs={6}>
               <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Graph Algorithm Based Travel Planner</h2>
-                <p>
-                  Collaborated with a cross-functional team to develop a system
-                  that enables users to strategically plan routes on 3
-                  priorities. Implemented 2 search algorithms using Python,
-                  Djikstra and Breadth First, computing results based cost,
-                  time, & directness.
-                </p>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Decision Tree</h2>
-                <p>
-                  Designed & developed a Python-based Decision Tree algorithm
-                  with a team of 2, achieving a prediction accuracy {">"}85%.
-                  Facilitated agile practices such as standups to share
-                  accomplishment, status, and stuck points.
-                </p>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>NLP for “Fake News” Classification</h2>
+                <img height="200px" src={nlpImage} alt="NLP Project" />
+                <h2 className={styles.project_title}>
+                  NLP for “Fake News” Classification
+                </h2>
                 <p>
                   Applied 6 ML algorithms (SVM, kNN, Naive Bayes, Decision Tree,
                   Adaboost, & Bagging) to classify articles as real/fake news.
@@ -49,8 +36,8 @@ const Projects = () => {
             </Grid>
             <Grid item xs={6}>
               <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Shell</h2>
+                <img height="200px" src={shellImage} alt="Shell Project" />
+                <h2 className={styles.project_title}>Shell</h2>
                 <p>
                   Implemented a custom Unix shell using C, utlizing system calls
                   and process management. Developed features such as command
@@ -62,31 +49,70 @@ const Projects = () => {
 
             <Grid item xs={6}>
               <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Seam Carver</h2>
-                <p>
-                  Employed dynamic programming to create an image resizing
-                  program that reduces the width of a given image. Stored pixel
-                  importance values in 2D array to minimize data loss.
-                </p>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Search Engine Implementation</h2>
+                <img height="200px" src={searchImage} alt="Search Project" />
+                <h2 className={styles.project_title}>
+                  Search Engine Implementation
+                </h2>
                 <p>
                   Implemented an Indexer to parse XML documents and determine
                   authority. Programmed a Ranking Function to produce search
                   results based on Term Frequency and Inverse Document
                   Frequency.
                 </p>
+                <br />
               </Box>
             </Grid>
             <Grid item xs={6}>
               <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Database</h2>
+                <img
+                  height="200px"
+                  src={plannerImage}
+                  alt="Travel Planner Project"
+                />
+                <h2 className={styles.project_title}>
+                  Graph Algorithm Based Travel Planner
+                </h2>
+                <p>
+                  Collaborated with a cross-functional team to develop a system
+                  that enables users to strategically plan routes on 3
+                  priorities. Implemented 2 search algorithms using Python,
+                  Djikstra and Breadth First, computing results based cost,
+                  time, & directness.
+                </p>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box className={styles.zoom}>
+                <img height="200px" src={seamImage} alt="Seam Carver Project" />
+                <h2 className={styles.project_title}>Seam Carver</h2>
+                <p>
+                  Employed dynamic programming to create an image resizing
+                  program that reduces the width of a given image. Stored pixel
+                  importance values in 2D array to minimize data loss.
+                </p>
+                <br />
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box className={styles.zoom}>
+                <img
+                  height="200px"
+                  src={treeImage}
+                  alt="Decision Tree Project"
+                />
+                <h2 className={styles.project_title}>Decision Tree</h2>
+                <p>
+                  Designed & developed a Python-based Decision Tree algorithm
+                  with a team of 2, achieving a prediction accuracy {">"}85%.
+                  Facilitated agile practices such as standups to share
+                  accomplishment, status, and stuck points.
+                </p>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box className={styles.zoom}>
+                <img height="200px" src={dbImage} alt="Database Project" />
+                <h2 className={styles.project_title}>Database</h2>
                 <p>
                   Developed a multithreaded server database, enabling concurrent
                   client connections. Implemented fine grained locking for
@@ -96,8 +122,17 @@ const Projects = () => {
             </Grid>
             <Grid item xs={6}>
               <Box className={styles.zoom}>
-                <img height="200px" src={projImage} alt="Project Picture" />
-                <h2>Interpreter</h2>
+                <br />
+                <br />
+                <br />
+                <img
+                  width="500px"
+                  src={interpreterImage}
+                  alt="Interpreter Project"
+                />
+                <br />
+                <br />
+                <h2 className={styles.project_title}>Interpreter</h2>
                 <p>
                   Created an interpreter to read, parse, and process user
                   input.Utilized extensive pattern matching and error checking.
